72bed8dc0996d44434e6ce492e741ab1cc2a4b22
Wan <w6hu@uwaterloo.ca>
2011-11-29 22:52:07 -0500

    worked a little bit on student

--- Student.cc
+++ Student.cc
@@ -1,8 +1,11 @@
 #include "Student.h"
+#include "MPRNG.h"
+
+extern MPRNG mprng;
 
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
-                  unsigned int maxPurchases ) :
-  prt(prt), nameServer(nameServer), cardOffice(cardOffice)
+        unsigned int maxPurchases ) :
+    prt(prt), nameServer(nameServer), cardOffice(cardOffice)
 {
     this->id = id;
     this->maxPurchases = maxPurchases;
@@ -10,7 +13,35 @@ Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffic
 
 void Student::main()
 {
+    int r = mprng()% MaxPurchases +1;   //select random number of bottles to buy
+    VendingMachine::Flavours favourite = (VendingMachine::Flavours)mprng()%4;
+    WATCard* card = new WATCard();
+    cardOffice.create(prt, 5, card);            //create initial WATCard
+    VendingMachine * myVm = getMachine(id);     //get a vending machine
 
-}
+    for (int i = 0; i < maxPurchases; i++){
+        yield(mprng()%10 + 1);              //yield before buying
+        VendingMachine::Status status;
+        do {
+            status = myVm.buy(flavour, card);
+            switch ( status ){
+                case VendingMachine::BUY:
+                    break;
+                case VendingMachine::STOCK:
+                    vm = nameServer.getMachine(id);     //get a new vm when out of stock
+                    break;
+                case VendingMachine::FUNDS:
+                    try{
+                        cardOffice.transfer(id, 5 + sodaCost, card); //may have to block until finished
+                    }catch WATCardOffice::LOST{                     //lost card
+                        delete(card);
+                        card = new WATCard();
+                        cardOffice.create(prt, 5, card);
+                    }
+                    //block student
+                    break;
+            } 
+        }while (status == BUY);
+    }
 
 
