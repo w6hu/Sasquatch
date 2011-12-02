201d32cc9164f3c30bb67682d4fadab659f315d8
Michael Liu <m27liu@engmail.uwaterloo.ca>
2011-12-01 20:42:00 -0500

    added implementation for bank

--- Bank.cc
+++ Bank.cc
@@ -3,15 +3,25 @@
 Bank::Bank( unsigned int numStudents )
 {
     this->numStudents = numStudents;
+
+    accounts = new unsigned int[numStudents];
+    for (unsigned int i = 0; i < numStudents; i++) {
+        accounts[i] = 0;
+    }
+}
+
+Bank::~Bank()
+{
+    delete[] accounts;
 }
 
 void Bank::deposit( unsigned int id, unsigned int amount )
 {
-
+    accounts[id] += amount;
 }
 
 void Bank::withdraw( unsigned int id, unsigned int amount )
 {
-
+    accounts[id] -= amount;
 }
 
--- Bank.h
+++ Bank.h
@@ -5,8 +5,10 @@
 
 _Monitor Bank {
     unsigned int numStudents;
+    unsigned int *accounts;
   public:
     Bank( unsigned int numStudents );
+    ~Bank();
     void deposit( unsigned int id, unsigned int amount );
     void withdraw( unsigned int id, unsigned int amount );
 };
